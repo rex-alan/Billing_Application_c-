ac0064f4234779419635abb585fc23a174c62b51
rex-alan <rexalan1174@gmail.com>
2022-12-28 00:23:16 +0530

    c1

---
+++ Service_Centere.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biling_Application1
+{
+    class Service_Centere
+    {
+        public string Name;     //we can declare a variable in public without encapsulate
+        public string Address;
+
+        public override string ToString()
+        {
+            return string.Format(" \t Service centere name : {0} | Address : {1}", Name, Address);
+        }
+    }
+}
