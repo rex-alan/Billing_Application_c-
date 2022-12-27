a693f4bc1404d0ef99eea634d3ea2a1726247433
rex-alan <rexalan1174@gmail.com>
2022-12-27 23:59:26 +0530

    Header code

---
+++ Header.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biling_Application1
+{
+    class Header
+    {
+        int _bill_No;
+        public int Bill_No
+        {
+            get { return _bill_No; }
+            set { _bill_No = value; }
+        }
+
+        string _date;
+        public string Date
+        {
+            get { return _date; }
+            set { _date = value; }
+        }
+
+        public override string ToString()
+        {
+            return string.Format("Bill No : {0} \t\t\t\t\t\t\t\t\t\t      Date : {1}", this.Bill_No, this.Date);
+        }
+    }
+}
